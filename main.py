63a4784693be7dd8651afa6bb23f6a8f36086a58
Memofting <pappy.mot@gmail.com>
2021-04-19 19:46:56 +0500

    update byte length decapitalize algo

--- main.py
+++ main.py
@@ -12,16 +12,9 @@ for c in string.ascii_uppercase:
 
 
 def encode(cap_file, decap_file, mask_file) -> None:
-    """
-    counter <= 19 for this text
-    :param cap_file:
-    :param decap_file:
-    :param mask_file:
-    :return:
-    """
     txt_bytes = cap_file.read()
     counter = 1
-    for byte in txt_bytes:
+    for i, byte in enumerate(txt_bytes, start=1):
         if counter == 256:
             mask_file.write(b'\x00')
             counter = 1
@@ -34,22 +27,22 @@ def encode(cap_file, decap_file, mask_file) -> None:
 
 def decode(cap_file, decap_file, mask_file) -> None:
     txt_bytes = decap_file.read()
-    next_cap = 0
+    next_shift, next_cap = 0, 0
     for i, byte in enumerate(txt_bytes, start=1):
-        if i > next_cap:
-            while True:
-                count = mask_file.read(1)[0]
-                if count == 0:
-                    next_cap += 256
-                else:
-                    next_cap += count - 1
-                    break
-        elif i == next_cap:
-            byte_s = ord(b'A') + byte - ord(b'a')
-            if not 0 <= byte_s < 256:
-                print(i, chr(txt_bytes[i-1]), next_cap, byte, byte_s)
-                return
-            byte = byte_s
+        while True:
+            if i <= next_cap:
+                break
+            buf = mask_file.read(1)
+            if not buf:
+                break
+            count = buf[0]
+            if count == 0:
+                next_shift += 255
+            else:
+                next_cap = next_shift + count
+                break
+        if i == next_cap:
+            byte = ord(b'A') + byte - ord(b'a')
         cap_file.write(bytes([byte]))
 
 
